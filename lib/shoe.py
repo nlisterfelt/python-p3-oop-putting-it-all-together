6e12b290f93ae0ddde04cab3138ca6240e4b11eb
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-30 11:56:43 -0500

    property

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand='', size='', condition=''):
+            self.brand = brand   
+            self.size = size
+
+    def set_size(self, size):
+        if(type(size)==int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    def get_size(self):
+        return self._size
+    
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self.condition = 'New'
+
+shoe = Shoe("Adidas", 9)
+shoe.size = 'ten'
